c11b2d75595b58cb6a33363c21b08530ebdfc783
minmmmmin <kotomi.hinahima@gmail.com>
2024-12-21 10:16:40 +0900

    画面切り替えつけてみた！！！

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Joke from "./components/Joke";
 import News from "./components/News";
 
+function Home() {
+  return (
+    <div>
+      <h1>あほっこ動物</h1>
+      <News />
+    </div>
+  );
+}
+
+function NewsPage() {
+  return (
+    <div>
+      <h1>あほジェネレータ</h1>
+      <Joke />
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
+    <Router>
       <Header />
-      <h1>あほっこ動物</h1>
-      <div>
-        <Joke />
-      </div>
-      <div className="news">
-        <News />
+      <nav>
+        <Link to="/">ホーム</Link>
+        <Link to="/news">ミニゲーム</Link>
+      </nav>
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<NewsPage />} />
+        </Routes>
       </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
