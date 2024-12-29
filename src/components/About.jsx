cfe489e0676e2e13a0661109a7957f27b39bf6c9
minmmmmin <kotomi.hinahima@gmail.com>
2024-12-29 20:20:36 +0900

    とりあえずあほな紹介を書いた。

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,15 @@ import CustomLoadingButton from "./LoadingButton";
 export default function About() {
   return (
     <>
-    <p>あほっこ動物についての紹介を書く</p>
+      <p>
+        あほっこ動物は、<strong>あほな動物たち</strong>のことを指します。
+        不定期でさまざまな個性豊かな動物たちが誕生します。
+        この何とも言えない表情が魅力的。
+      </p>
+      <p>
+        名前がある動物もいれば、ない動物もいます。
+        どの動物も、見ているだけで癒やされること間違いなし！
+      </p>
     </>
   );
 }
