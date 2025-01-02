ca5c1c1dfab76d86cd3b070fb3020ab63f7b69b0
minmmmmin <kotomi.hinahima@gmail.com>
2025-01-02 20:08:28 +0900

    ずかん完成！！！！

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,27 +1,30 @@
 export default function Animal() {
   const imagePaths = [
-    "images/hebi.PNG",
-    "images/hituji.PNG",
-    "images/inoshishi.PNG",
-    "images/inu.PNG",
-    "images/kumasan.png", 
-    "images/mogumogurisu.png", 
-    "images/monkey.PNG",
-    "images/mouse.PNG",
-    "images/pig.png",
-    "images/piyo.PNG",
-    "images/tanuki.png",
-    "images/tatu.PNG",
-    "images/tora.PNG",
-    "images/uma.PNG",
-    "images/usagi.PNG",
-    "images/usi.PNG",
+    { src: "images/hebi.PNG", caption: "へび" },
+    { src: "images/hituji.PNG", caption: "ひつじ" },
+    { src: "images/inoshishi.PNG", caption: "いのしし" },
+    { src: "images/inu.PNG", caption: "いぬ" },
+    { src: "images/kumasan.png", caption: "くまさん" },
+    { src: "images/mogumogurisu.png", caption: "もぐもぐりす" },
+    { src: "images/monkey.PNG", caption: "うきうきもんきー" },
+    { src: "images/mouse.PNG", caption: "ねずみ" },
+    { src: "images/pig.png", caption: "ぶた" },
+    { src: "images/piyo.PNG", caption: "piyo" },
+    { src: "images/tanuki.png", caption: "たぬき" },
+    { src: "images/tatu.PNG", caption: "たつ" },
+    { src: "images/tora.PNG", caption: "とらくん" },
+    { src: "images/uma.PNG", caption: "うま" },
+    { src: "images/usagi.PNG", caption: "うきうきらびっと" },
+    { src: "images/usi.PNG", caption: "うし" },
   ];
 
   return (
-    <div>
-      {imagePaths.map((path, index) => (
-        <img key={index} src={path} alt={`animal-${index}`} className="animal-image"  />
+    <div className="animal-gallery">
+      {imagePaths.map((item, index) => (
+        <div key={index} className="animal-item">
+          <img src={item.src} alt={item.caption} className="animal-image" />
+          <p className="animal-caption">{item.caption}</p>
+        </div>
       ))}
     </div>
   );
