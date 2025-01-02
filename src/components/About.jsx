2ad0c5ffcffe6d0bda33962af36026c6ce99baa8
minmmmmin <kotomi.hinahima@gmail.com>
2025-01-02 19:21:27 +0900

    aboutをきれいにした

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,12 @@
-import { useState, useEffect } from "react";
-import CustomLoadingButton from "./LoadingButton";
-
 export default function About() {
   return (
     <>
-      <p>
+      <p className="about-text">
         あほっこ動物は、<strong>あほな動物たち</strong>のことを指します。
-        不定期でさまざまな個性豊かな動物たちが誕生します。
-        この何とも言えない表情が魅力的。
-      </p>
-      <p>
-        名前がある動物もいれば、ない動物もいます。
-        どの動物も、見ているだけで癒やされること間違いなし！
+        <br />不定期でさまざまな個性豊かな動物たちが誕生します。
+        <br />この何とも言えない表情が魅力的。
+        <br />名前がある動物もいれば、ない動物もいます。
+        <br />どの動物も、見ているだけで癒やされること間違いなし！
       </p>
     </>
   );
