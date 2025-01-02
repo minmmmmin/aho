afc113acc147bc47cedef90d017309750516f58a
minmmmmin <kotomi.hinahima@gmail.com>
2025-01-02 19:57:45 +0900

    とりあえず動物を出した

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,7 +1,28 @@
 export default function Animal() {
+  const imagePaths = [
+    "images/hebi.PNG",
+    "images/hituji.PNG",
+    "images/inoshishi.PNG",
+    "images/inu.PNG",
+    "images/kumasan.png", 
+    "images/mogumogurisu.png", 
+    "images/monkey.PNG",
+    "images/mouse.PNG",
+    "images/pig.png",
+    "images/piyo.PNG",
+    "images/tanuki.png",
+    "images/tatu.PNG",
+    "images/tora.PNG",
+    "images/uma.PNG",
+    "images/usagi.PNG",
+    "images/usi.PNG",
+  ];
+
   return (
-    <>
-    <p>coming soon…</p>
-    </>
+    <div>
+      {imagePaths.map((path, index) => (
+        <img key={index} src={path} alt={`animal-${index}`} className="animal-image"  />
+      ))}
+    </div>
   );
 }
