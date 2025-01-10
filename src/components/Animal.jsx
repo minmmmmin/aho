41ad964635227be77100a1d2d13c64fedd18dd68
minmmmmin <kotomi.hinahima@gmail.com>
2025-01-10 16:00:25 +0900

    ずかんの更新

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+
 export default function Animal() {
   const imagePaths = [
     { src: "images/mouse.PNG", caption: "ねずみ" },
@@ -19,13 +22,47 @@ export default function Animal() {
   ];
 
   return (
-    <div className="animal-gallery">
-      {imagePaths.map((item, index) => (
-        <div key={index} className="animal-item">
-          <img src={item.src} alt={item.caption} className="animal-image" />
-          <p className="animal-caption">{item.caption}</p>
-        </div>
+    <Grid
+      container
+      spacing={2}
+      style={{
+        paddingLeft: "16px",
+        paddingRight: "16px",
+        margin: "0 auto", // 中央揃え
+        maxWidth: "1200px", // 全体の幅を制限
+      }}
+    >
+      {imagePaths.map((image, index) => (
+        <Grid
+          item
+          xs={12}
+          sm={3}
+          key={index}
+          style={{
+            textAlign: "center",
+            marginBottom: "20px", // 下部の余白を追加
+          }}
+        >
+          <img
+            src={image.src}
+            alt={image.caption}
+            style={{
+              width: "70%", // 画像の幅を指定
+              height: "auto", // アスペクト比を維持
+            }}
+          />
+          <Typography
+            variant="body1"
+            fontFamily="Hanazome"
+            style={{
+              marginTop: "5px",
+              fontSize: "1.5rem",
+            }}
+          >
+            {image.caption}
+          </Typography>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
