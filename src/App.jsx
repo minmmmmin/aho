38860a8a7a43b7a452c4ea4cb89ba9251ebccdb6
minmmmmin <kotomi.hinahima@gmail.com>
2025-12-09 18:39:55 +0900

    fix: lint修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Joke from "./components/Joke";
